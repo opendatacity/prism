5edbf29b036565bb882158d39170f9457b763a05
ffalt <ffalt@server.ffalt>
2013-07-05 08:44:57 +0200

    scroll the info console

--- assets/js/index.js
+++ assets/js/index.js
@@ -89,85 +89,6 @@ RaPath.prototype = {
 	}
 };
 
-function Typing() {
-//slightly modified from https://github.com/rachstock/typing-animation
-}
-
-Typing.prototype = {
-	lines: '',
-	container: null,
-	insertPoint: null,
-	para: null,
-
-	beginTyping: function (lines, parent) {
-		this.lines = lines;
-
-		// Create the typer div
-		this.container = $(parent);//$(document.createElement('div'));
-//		this.container.attr('id', 'typer');
-//		parent.append(this.container);
-
-		this.container.empty();
-		// Create the 'insertion point'
-		this.insertPoint = $(document.createElement('span'));
-		this.insertPoint.attr('id', 'insert-point');
-
-		// Create our paragraph tags
-		for (var i = 0, l = lines.length; i < l; i++) {
-			var p = $(document.createElement('p'));
-			p.attr('class', 'typer');
-			p.html('<span class="typing"></span>');
-			this.container.append(p);
-		}
-
-//		this.insertPoint.css('height', fontSize);
-
-		// Select first paragraph, add the insertion point, start typing
-		var p = this.container.find('p:first');
-		p.append(this.insertPoint);
-		var caller = this;
-		setTimeout(function () {
-			caller.typeLine(p, 0);
-		}, 2000);
-	},
-
-	typeLine: function (p, index) {
-		var span = p.find('span.typing');
-		var line = this.lines[index];
-		// Begin typing line
-		this.typeLetter(p, index, 0, span, line);
-	},
-
-	typeLetter: function (p, lineIndex, letterIndex, span, line) {
-		// add the letter
-		span.append(line[letterIndex]);
-		var caller = this;
-		if (letterIndex + 1 < line.length) {
-			// Add another letter (after a delay)
-			setTimeout(function () {
-					caller.typeLetter(p, lineIndex, letterIndex + 1, span, line);
-				}, Math.floor((Math.random() * 100) + 50)
-			);
-		} else {
-			// We've reached the end of the line, callback after a short delay
-			setTimeout(function () {
-				caller.finishLine(p, lineIndex)
-			}, 500);
-		}
-	},
-
-	finishLine: function (p, index) {
-		// When we've finished a line of type, start a new one
-		if (index + 1 < this.lines.length) {
-			p = p.next();
-			this.insertPoint.remove().appendTo(p);
-			this.typeLine(p, index + 1);
-		} else {
-			this.insertPoint.remove();
-		}
-	}
-};
-
 function init() {
 
 	map = new L.Map("map", { center: DEFAULT_POINT, zoom: 3});
@@ -210,9 +131,11 @@ function init() {
 		this._div.html(h);
 	};
 	info.append = function (txt) {
-		if (this._div.children().length > 3)
+		if (this._div.children().length > 30)
 			this._div.children().first().remove();
 		this._div.append('<p>' + txt + '</p>');
+
+		this._div.scrollTop(this._div.prop("scrollHeight"));
 	};
 	info.clear = function () {
 		this._div.empty();
